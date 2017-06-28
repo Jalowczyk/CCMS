8679ef4eaaf28cb50410f650991a557b664a7003
Iwo <iwo.tyszkowski@gmail.com>
2017-06-28 10:11:39 +0200

    folder added with attendances file

---
+++ model/attendance.py
@@ -0,0 +1,41 @@
+
+class Attendance:
+    '''
+    An object containing informations about attendance.
+    '''
+
+    ATTENDANCES = []
+
+    def __init__(self, date, student, is_present):
+        '''
+        Constructs an Attendance object.
+        Raises ValueError is type of any argument is incorrect.
+
+        Args:
+            date (:obj: 'date'): date of attendance
+            student (:obj: '?type?'): Student objeci
+            is present (bool): True for present, False for absent
+
+        Returns:
+            void
+        '''
+        self.date = date
+        self.student = student
+        self.is_present = is_present
+
+    def add():
+
+
+
+
+
+Class Attendance
+
+attributes
+self.date: Date obj
+self.student: Student obj
+self.is_present: bool
+attendances: list
+
+__init__: void
+add(): void
