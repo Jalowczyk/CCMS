e06e5fb6b68ca87d21e34c9629b8c9b064dcd30d
Szymon Wojtas <szymon.wojtas@gmail.com>
2017-06-28 11:01:09 +0200

    add assignment initizialer

--- model/assignment.py
+++ model/assignment.py
@@ -1,16 +1,26 @@
 class Assignment:
+    """Represents assignment object
+
+    class atribute:
+        assignment: list
+            list of assignments objects
+
+    instance atributes:
+        title: str
+        description: str
+        max_grade: int
+        submissons: list
+            list of submissions objects, connected to assignmnet
+    """
 
     assignments = []
 
     def __init__(self, title, description, max_grade):
+        """Initialize assignment object
+
+        returns: None
+        """
         self.title = title
         self.description = description
         self.max_grade = max_grade
         self.submissons = []
-
-    @classmethod
-    def add_assignment(cls, assignment):
-        cls.assignments.append(assignment)
-
-    def add_submission(self, submisson):
-        self.submissons.append(submisson)
