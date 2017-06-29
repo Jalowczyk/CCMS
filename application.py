b1d72d579201da1be17b2601454853146daac5e3
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-06-29 11:15:40 +0200

    Add method log_in in Application class

--- application.py
+++ application.py
@@ -14,3 +14,14 @@ class Application:
         self.logged_user = {"logged_user": None}
 
     
+
+    def log_in(self):
+        email, password = self.user_input.get_login_input()
+        codecoolers = Manager.get_managers() + Staff.get_staff() + Mentor.get_mentors() + Student.get_students()
+
+        for codecooler in codecoolers:
+            if email == codecooler.get_email() and password == codecooler.get_password():
+                return codecooler
+            else:
+                self.view.view_error("\nWrong login or password!\n")
+                return None
