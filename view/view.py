ee7c14ed544301f566ba2f94fd8242d5f5366673
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-06-29 14:52:35 +0200

    Add show_assignments method in View class

--- view/view.py
+++ view/view.py
@@ -5,7 +5,12 @@ class View:
         for index, option in enumerate(options):
             print("{}{} {}".format(index + 1, ".", option))
 
-    def view_codecooler(self, codecooler):
+    def show_codecooler(self, codecooler):
 
-        print("{} {}: {}".format(codecooler.get_first_name(), codecooler.get_last_name(),
-                        get_email()))
+        print("{} {}: {}".format(codecooler.get_first_name(),
+                                 codecooler.get_last_name(), get_email()))
+
+    def show_assignments(self, assignments):
+
+        for index, assignment in enumerate(assignments):
+            print("{}{} {}".format(index + 1, ".", assignment))
