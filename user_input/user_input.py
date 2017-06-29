8d7d6772b3ac466044f2f9a02fef5da6a7c7811e
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-06-29 17:36:36 +0200

    Add get_boolean_input method in UserInput class

--- user_input/user_input.py
+++ user_input/user_input.py
@@ -1,3 +1,5 @@
+import getpass
+
 class UserInput:
     """
     Creates UserInput object.
@@ -5,20 +7,36 @@ class UserInput:
 
     def get_option(self, options):
         """
-        Returns the option from the options list depeding on the user input.
+        Returns the option from the options list depending on the user input.
         Parameters:
         options: list
 
         Returns:
-        options[user_decision - 1]: str
+        user_decision: str
         """
 
-        user_input_number = None
+        user_input_number = int(input("\nPick an option (number): "))
         while user_input_number not in range(len(options)):
             user_input_number = int(input("\nPick an option (number): "))
 
         user_decision = options[user_input_number - 1]
         return user_decision
 
+    def get_boolean_input(self):
+        """
+        Return boolean depending on the user input.
+
+        Returns:
+        boolean: True/False
+        """
+
+        user_input_bool = input("\nPick an option (y/n): ").lower()
+        while user_input_bool not in ["y", "n"]:
+            user_input_bool = input("\nPick an option (y/n): ").lower()
+
+        if user_input_bool == "y":
+            return True
+        else:
+            return False
 
     
