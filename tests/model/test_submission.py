cd89ff9d1f5bcdc05bb0575eb8b911feeea989d8
Marek Frankowicz <frankowicz.marek@gmail.com>
2017-06-29 14:07:36 +0200

    Add submission tests for grades

--- tests/model/test_submission.py
+++ tests/model/test_submission.py
@@ -21,3 +21,24 @@ class TestSubmission(unittest.TestCase):
         submission_data = (Mock(Student), Mock(Assignment), "")
         with self.assertRaises(ValueError):
             Submission(*submission_data)
+
+    def test_if_set_grade_sets_value(self):
+        submission_data = (Mock(Student), Mock(Assignment), "solution")
+        submission = Submission(*submission_data)
+        grade = 10
+        submission.set_grade(grade)
+        self.assertEqual(submission.get_grade(), grade)
+
+    def test_if_set_grade_throws_exception_when_grade_is_not_numeric(self):
+        submission_data = (Mock(Student), Mock(Assignment), "solution")
+        submission = Submission(*submission_data)
+        grade = "ab"
+        with self.assertRaises(TypeError):
+            submission.set_grade(grade)
+
+    def test_if_set_grade_throws_exception_when_grade_is_LT0(self):
+        submission_data = (Mock(Student), Mock(Assignment), "solution")
+        submission = Submission(*submission_data)
+        grade = -1
+        with self.assertRaises(ValueError):
+            submission.set_grade(grade)
