e6f8b25b9c39a6fec204741d07e356a6f9aaff51
Szymon Wojtas <szymon.wojtas@gmail.com>
2017-06-29 09:37:53 +0200

    edit_codecooler_action added

--- controller/codecooler_controller.py
+++ controller/codecooler_controller.py
@@ -28,4 +28,25 @@ class CodecoolerController:
         elif person_role == "student":
             student = Student(self.user_input.get_codecooler_data())
             student.add()
-    
+
+    def edit_codecooler_action(self, person_role):
+        if person_role == "mentor":
+            mentors = Mentor.get_mentors()
+            self.view.show_codecoolers(mentors)
+            option = self.user_input.get_record_by_index(len(mentors))
+            mentor = mentors[option]
+            data = self.user_input.get_codecooler_data()
+            mentor.set_name = data["name"]
+            mentor.set_surname = data["surname"]
+            mentor.set_email = data["email"]
+            mentor.set_password = data["password"]
+        elif person_role == "student":
+            students = Student.get_students()
+            self.view.show_codecoolers(students)
+            option = self.user_input.get_record_by_index(len(students))
+            student = students[option]
+            data = self.user_input.get_codecooler_data()
+            student.set_name = data["name"]
+            student.set_surname = data["surname"]
+            student.set_email = data["email"]
+            student.set_password = data["password"]
