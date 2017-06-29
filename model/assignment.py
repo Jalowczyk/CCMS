1775573ff4d13f0467e017285afd81db6b22f8b8
Szymon Wojtas <szymon.wojtas@gmail.com>
2017-06-29 12:54:03 +0200

    add get_assignments_list method to assignments.py in model

--- model/assignment.py
+++ model/assignment.py
@@ -34,7 +34,7 @@ class Assignment:
         Returns:
             None
         """
-        self.assignments.append(assignment)
+        self.assignments.append(self)
 
     def add_submission(self, submisson):
         """Add submisson connected to assignment object
@@ -78,3 +78,12 @@ class Assignment:
             submisson: list of submisson class objects
         """
         return self.submissons
+
+    @classmethod
+    def get_assignments_list(cls):
+        """Gets assignments atribute of class
+
+        Returns
+            assignments = list of assignments class objects
+        """
+        return cls.assignments
