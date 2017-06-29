7ec846b32aecd7518892a2b5b8b0c6fbd92c925b
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-06-29 12:59:04 +0200

    Add docstrings in Application class

--- application.py
+++ application.py
@@ -5,7 +5,20 @@ from view.view import View
 
 
 class Application:
-    
+    """
+    Creates Application obj.
+    Class attributes:
+    roles: dict
+    options: list
+
+    Instance attributes:
+    is_running: bool
+    session: dict
+    menu: None/Menu obj
+    user_input: UserInput obj
+    view: View obj
+    path: str
+    """
 
     roles = {"Manager": ManagerMenu, "Staff": StaffMenu, "Mentor": MentorMenu,
              "Student": StudentMenu}
@@ -13,6 +26,9 @@ class Application:
     options = ["log in", "exit"]
 
     def __init__(self):
+        """
+        Creates Application obj.
+        """
         self.is_running = True
         self.session = {"logged_user": None}
         self.menu = None
@@ -22,6 +38,9 @@ class Application:
 
 
     def handle_login(self):
+        """
+        Enables to log in or exit program.
+        """
 
         while not self.session["logged_user"]:
             user_option = self.input.get_option(self.options)
@@ -32,6 +51,9 @@ class Application:
                 break
 
     def handle_menu(self):
+        """
+        Directs the user to the proper menu, depending on the user role.
+        """
 
         if self.session["logged_user"]:
             role = self.session["logged_user"].__class__.__name__
@@ -41,12 +63,24 @@ class Application:
             self.menu.handle_menu(user_choice)
 
     def run(self):
+        """
+        Entry method for the main module.
+        """
 
         while is_running:
             handle_login()
             handle_menu()
 
     def log_in(self):
+        """
+        Compares user email and password input to objects email and
+        password attributes, and if they are ok, returns user object, otherwise
+        returns None.
+
+        Returns:
+        codecooler: obj
+        None
+        """
         email, password = self.user_input.get_login_input()
         codecoolers = Manager.get_managers() + Staff.get_staff() + Mentor.get_mentors() + Student.get_students()
 
