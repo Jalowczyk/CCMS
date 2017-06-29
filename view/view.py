9d70f4dc9cfd591fe643794a73a116e6d77eb34f
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2017-06-29 14:33:00 +0200

    Add view_codecooler method in View class

--- view/view.py
+++ view/view.py
@@ -5,4 +5,7 @@ class View:
         for index, option in enumerate(options):
             print("{}{} {}".format(index + 1, ".", option))
 
-    
+    def view_codecooler(self, codecooler):
+
+        print("{} {}: {}".format(codecooler.get_first_name(), codecooler.get_last_name(),
+                        get_email()))
